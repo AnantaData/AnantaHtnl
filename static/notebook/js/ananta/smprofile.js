0992359bb5e2a8c50a4bb466e03eb3b3227cef2f
damith senanayake <dracusds123@gmail.com>
2015-02-07 17:21:47 +0530

    temporary changes adding

--- static/notebook/js/ananta/smprofile.js
+++ static/notebook/js/ananta/smprofile.js
@@ -1,3 +1,5 @@
+
+
 var IPython = (function (IPython) {
 
     var SMProfile = function (kernel, options) {
@@ -6,7 +8,8 @@ var IPython = (function (IPython) {
 
         this.gui_type = 'smp';
         this.profileData = {
-            algorithm: ""
+            algorithm: "",
+            response_var:"churn"
 
         };
 
@@ -23,6 +26,7 @@ var IPython = (function (IPython) {
 
     SMProfile.prototype.create_element = function () {
         IPython.Profile.prototype.create_element.apply(this, arguments);
+
         this.profileheading.text('Supervised Mining Profile');
         this.profileheading[0].style.color="#04B486";
 
@@ -43,10 +47,12 @@ var IPython = (function (IPython) {
         else if (algorithm == 'svm') {
             alg = 'TrainSVMStep()';
         }
-        var code = 'from ananta_base.base import *' +
+        var code ='print "hello world"'; /*'from ananta_base.base import *' +
             '\nfrom ananta_base.mining import supervised_mining as sm' +
-            '\nfrom sklearn.metrics import roc_auc_score' +
-            '\nsmp1 = sm.SupervisedMiningProfile()' +
+            //'\nfrom sklearn.metrics import roc_auc_score' +
+            '\nprint "imports done"'+
+            '\nsmp1 = sm.SupervisedMiningProfile("'+profileData.response_var+'")' +
+            '\nprint "mining profile created" '+
             '\ns1= sm.' + alg + '' +
             '\ns2=sm.PredictStep()' +
             '\nsmp1.addStep(s1)' +
@@ -56,10 +62,8 @@ var IPython = (function (IPython) {
             '\ndf = projects.pred_y' +
             '\nprint df' +
             '\ndf.tofile("a.csv", sep=",")' +
-            '\nif projects.Test_Y != None:' +
-            '\n\tprojects.roc_auc_score = roc_auc_score(Test_Y,pred_y)' +
             '\nprint projects.roc_auc_score'+
-            '';
+            '';*/
         return code;
 
     }
@@ -68,85 +72,3 @@ var IPython = (function (IPython) {
 
     return IPython;
 }(IPython));
-
-/* var get_smp_code= function(nb,algorithm) {
- var alg="";
- alert(algorithm)
- if (algorithm=='logit'){
- alg='TrainLogitStep()';
- }
- else if(algorithm == 'ranfor') {
- alg='TrainRanforStep()';
- }
- else if(algorithm=='svm'){
- alg='TrainSVMStep()';
- }
- var code = 'from ananta_base.base import *' +
- '\nfrom ananta_base.mining import supervised_mining as sm'+
- '\nsmp1 = sm.SupervisedMiningProfile()' +
- '\ns1= sm.'+alg+''+
- '\ns2=sm.PredictStep()'+
- '\nump1.addStep(s1)' +
- '\nump1.addStep(s2)'+
- '\nump1.execute(projects)' +
- //'\ndf = projects.data.describe()' +
- '\ndf = projects.data' +
- '\nprint df' +
- '\ndf.tofile("a.csv", sep=",")' +
- '';
- nb.set_text(code);
-
- }
-
-
- get_smp_code(this, this.algorithm);
-
- var nb = this;
- this.b1.click(function(e){
- e.preventDefault();
- nb.settingsdialog.show_dialog(nb,get_smp_code);
- });
- this.b2.click(function(e){
- e.preventDefault();
- IPython.notebook.execute_cell();
- });
- this.b3.click(function(e){
- e.preventDefault();
- selectGrapgh(2);
- });
-
- this.b4.click(function(e){
- e.preventDefault();
- populate_eval();
- });
-
- this.profileheading.text('Supervised Mining Profile');
- };
-
-
- /* SMProfile.prototype.fromJSON = function (data) {
- if(data.gui_type ==='smp'){
- IPython.CodeCell.prototype.fromJSON.apply(this, arguments);
-
- this.fileName = data.fileName;
- this.fileType = data.fileType;
- this.fileLoc = data.fileLoc;
- }
- }; */
-
-/*
- SMProfile.prototype.toJSON = function () {
- var data = IPython.CodeCell.prototype.toJSON.apply(this);
-
- data.gui_type = this.gui_type;
- /*data.fileName = this.fileName;
- data.fileType = this.fileType;
- data.fileLoc = this.fileLoc;
- return data;
- };
-
-
- IPython.SMProfile = SMProfile;
-
- return IPython;
- }(IPython));*/
