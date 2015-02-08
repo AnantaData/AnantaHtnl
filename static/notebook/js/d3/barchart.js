5fdb770d2b89aba9ee8b00958b458f00a803af5f
tiroshan <tiroshanm@gmail.com>
2015-02-08 15:11:16 +0530

    update bar chart with popup

--- static/notebook/js/d3/barchart.js
+++ static/notebook/js/d3/barchart.js
@@ -19,6 +19,7 @@ function barChartSelectGrapgh(file,selection){
             //var property = object_properties[select_property];
             //console.log(property);
             data_array[j] = parseInt(object[selection]);
+            //console.log(data_array[j]);
         }
         //console.log(data_array);
         if (isNaN(data_array[0])){
@@ -36,6 +37,7 @@ function cratebarChar(values,column){
 
     // A formatter for counts.
     var formatCount = d3.format(",.0f");
+    var formatPercent = d3.format(".0%");
 
     var margin = {top: 10, right: 30, bottom: 30, left: 30},
         width = 400 ,//- margin.left - margin.right,
@@ -48,7 +50,40 @@ function cratebarChar(values,column){
         .domain([min_margin, max_margin])
         .range([0, width]);
 
-    // Generate a histogram using twenty uniformly-spaced bins.
+
+
+    //var svg = d3.select("#visualization-area").append("svg")
+    //    .attr("width", width + margin.left + margin.right)
+    //    .attr("height", height + margin.top + margin.bottom)
+    //    .append("g")
+    //    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    //
+    //var bar = svg.selectAll(".bar")
+    //    .data(data)
+    //    .enter().append("g")
+    //    .attr("class", "bar")
+    //    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+    //
+    //bar.append("rect")
+    //    .attr("x", 1)
+    //    .attr("width", function(){ if (x(data[0].dx) - 1<0) return 1; else return x(data[0].dx) - 1})
+    //    .attr("height", function(d) { return height - y(d.y); });
+    //
+    //bar.append("text")
+    //    .attr("dy", ".75em")
+    //    .attr("y", 6)
+    //    .attr("x", x(data[0].dx) / 2)
+    //    .attr("text-anchor", "middle")
+    //    .text(function(d) {
+    //        //console.log(d);
+    //        return formatCount(d.y);
+    //    });
+    //
+    //svg.append("g")
+    //    .attr("class", "x axis")
+    //    .attr("transform", "translate(0," + height + ")")
+    //    .call(xAxis);
+
     var data = d3.layout.histogram()
         .bins(x.ticks(100))
     (values);
@@ -61,21 +96,38 @@ function cratebarChar(values,column){
         .scale(x)
         .orient("bottom");
 
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(formatPercent);
+
     var svg = d3.select("#visualization-area").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return "<strong>Frequency:</strong> <span style='color:red'>" + formatCount(d.y) + "</span>";
+        })
+
+    svg.call(tip);
+
     var bar = svg.selectAll(".bar")
         .data(data)
         .enter().append("g")
         .attr("class", "bar")
-        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
+
 
     bar.append("rect")
         .attr("x", 1)
-        .attr("width", function(){ if (x(data[0].dx) - 1<0) return 1; else return x(data[0].dx) - 1})
+        .attr("width", x(data[0].dx) - 1)
         .attr("height", function(d) { return height - y(d.y); });
 
     bar.append("text")
@@ -83,10 +135,7 @@ function cratebarChar(values,column){
         .attr("y", 6)
         .attr("x", x(data[0].dx) / 2)
         .attr("text-anchor", "middle")
-        .text(function(d) {
-            console.log(d);
-            return formatCount(d.y);
-        });
+        .text(function(d) { return formatCount(d.y); });
 
     svg.append("g")
         .attr("class", "x axis")
