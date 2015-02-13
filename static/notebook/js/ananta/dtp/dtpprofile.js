1b229d9b172b1e90d31e2e95413cb0c16cf73619
Laksheen <cresclux@yahoo.com>
2015-02-13 07:31:28 +0530

    commented dtp

--- static/notebook/js/ananta/dtp/dtpprofile.js
+++ static/notebook/js/ananta/dtp/dtpprofile.js
@@ -1,4 +1,13 @@
+//----------------------------------------------------------------------------
+//  Copyright (C) 2015  The Ananta Development Team
+//
+//  Distributed under the terms of the BSD License.  The full license is in
+//  the file COPYING, distributed as part of this software.
+//----------------------------------------------------------------------------
 
+//============================================================================
+// Data Transformation Profile
+//============================================================================
 
 var IPython = (function (IPython) {
 
